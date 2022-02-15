393f87c002efb6756b6851297d89008d0a62516d
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2022-02-15 18:31:27 +0800

    Initial Commit

---
+++ Mini Vending Machine WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mini Vending Machine WatchKit Extension
+//
+//  Created by CGH on 2022/2/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mini Vending Machine WatchKit Extension/Mini_Vending_MachineApp.swift
@@ -0,0 +1,19 @@
+//
+//  Mini_Vending_MachineApp.swift
+//  Mini Vending Machine WatchKit Extension
+//
+//  Created by CGH on 2022/2/15.
+//
+
+import SwiftUI
+
+@main
+struct Mini_Vending_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
